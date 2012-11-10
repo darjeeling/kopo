193fc6418fcc9807dd4f49e80271a622c1277743
KwonHan Bae <darjeeling@gmail.com>
2012-11-10 20:10:51 +0900

    add utf8 encoding

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 import os, sys
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 from sqlalchemy import create_engine
 engine = create_engine('sqlite:///:memory:', echo=True)
